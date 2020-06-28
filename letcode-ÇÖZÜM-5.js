dc70f6179f84e4694bda4fdc54c4628e57e5247c
Cyber-Lorder <mamaodu_niang.1907@hotmail.com>
2020-06-28 22:48:58 +0300

    FİNAL-ÖDEVLERİ

---
+++ letcode-ÇÖZÜM-5.js
@@ -0,0 +1,10 @@
+var largestNumber = function(nums) {
+    nums.sort(cmp);
+    var result = nums.join('');
+    
+    return parseInt(result) === 0 ? '0' : result;
+};
+
+var cmp = function(a,b){
+    return ('' + a + b) > ('' + b + a) ? -1 : 1;
+}
